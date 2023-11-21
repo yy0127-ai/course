1b2f126e12f64de09cb9823d6ead262e74c8ca50
yy0127-ai <2259962117@qq.com>
2023-11-21 15:51:06 +0800

    1.增加dto层，用于controller和service层 2.增加ChapterDto 3.修改ChapterService，将返回Chapter改成返回ChapterDto

--- server/src/main/java/com/course/server/service/ChapterService.java
+++ server/src/main/java/com/course/server/service/ChapterService.java
@@ -2,19 +2,28 @@ package com.course.server.service;
 
 import com.course.server.domain.Chapter;
 import com.course.server.domain.ChapterExample;
+import com.course.server.dto.ChapterDto;
 import com.course.server.mapper.ChapterMapper;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class ChapterService {
     @Resource
     private ChapterMapper chapterMapper;
-    public List<Chapter> list(){
-        ChapterExample chapterExample = new ChapterExample();
-        chapterExample.createCriteria().andIdEqualTo("1");
-        return  chapterMapper.selectByExample(chapterExample);
+    public List<ChapterDto> list(){
+        List<Chapter> chapterList = chapterMapper.selectByExample(null);
+        List<ChapterDto> chapterDtoList = new ArrayList<ChapterDto>();
+        for(int i = 0, l = chapterList.size(); i < l; i++){
+            Chapter chapter = chapterList.get(i);
+            ChapterDto chapterDto = new ChapterDto();
+            BeanUtils.copyProperties(chapter, chapterDto);
+            chapterDtoList.add(chapterDto);
+        }
+        return chapterDtoList;
     }
 }
