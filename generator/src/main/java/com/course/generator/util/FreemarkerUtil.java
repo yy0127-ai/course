ebfe9366a3e0471676c2b40dd8b02a787d53e948
yy0127-ai <2259962117@qq.com>
2023-12-11 14:38:19 +0800

    1.制作FreemarkerUtil，简化生成器的使用 2.新增ServerGenerator，用于生成后端代码：controller service dto

---
+++ generator/src/main/java/com/course/generator/util/FreemarkerUtil.java
@@ -0,0 +1,31 @@
+package com.course.generator.util;
+
+import freemarker.template.Configuration;
+import freemarker.template.DefaultObjectWrapper;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FreemarkerUtil {
+    static String fltPath = "generator\\src\\main\\java\\com\\course\\generator\\ftl\\";
+    static Template temp;
+    public static void initConfig(String ftlName) throws IOException {
+        //读模板
+        Configuration cfg = new Configuration(Configuration.VERSION_2_3_29);
+        cfg.setDirectoryForTemplateLoading(new File(fltPath));
+        cfg.setObjectWrapper(new DefaultObjectWrapper(Configuration.VERSION_2_3_29));
+        temp = cfg.getTemplate(ftlName);
+    }
+    public static void generator(String fileName) throws IOException, TemplateException {
+        //生成模板
+        FileWriter fw = new FileWriter(fileName);
+        BufferedWriter bw = new BufferedWriter(fw);
+        temp.process(null, bw);
+        bw.flush();
+        fw.close();
+    }
+}
